a3cf8f499cb84fe3ecf2424baa2fa506277889ac
Gwydion <mathilda.kallouch@gmail.com>
2015-02-04 14:08:21 +0100

    no message

--- Sources/Assets/Scripts/Managers/Beziers/BezierPoint.cs
+++ Sources/Assets/Scripts/Managers/Beziers/BezierPoint.cs
@@ -14,15 +14,18 @@ public class BezierPoint : MonoBehaviour
 
         Gizmos.DrawSphere(transform.position ,_drawSphereRadius);
 
-        Gizmos.color = Color.grey;
+        
 
         if(_frontBracket != null)
         {
+            Gizmos.color = Color.grey;
             Gizmos.DrawLine(transform.position, _frontBracket.position);
+            Gizmos.color = Color.yellow;
             Gizmos.DrawSphere(_frontBracket.position, _drawSphereRadius);
         }
         if(_backBracket != null)
         {
+            Gizmos.color = Color.grey;
             Gizmos.DrawLine(transform.position, _backBracket.position);
             Gizmos.DrawSphere(_backBracket.position, _drawSphereRadius);
         }
