6bc78d136a869515421220f20416a1fa56f88d61
Kratorg <romain.simon.vivier@gmail.com>
2015-02-21 01:22:36 +0100

    remise à jour suite au merge

--- Sources/Assets/Scripts/Managers/Beziers/BezierCurve.cs
+++ Sources/Assets/Scripts/Managers/Beziers/BezierCurve.cs
@@ -8,12 +8,18 @@ public class BezierCurve : MonoBehaviour
     public bool _displayPoints = true;
     public bool _loop = true;
     public float _curveSpeed;
+    public float _lineWidth;
+    public Color _color;
 
+
+    private LineRenderer m_lineRenderer;
     private List<DraftRunner> m_runners;
 
     void Awake()
     {
         m_runners = new List<DraftRunner>();
+        m_lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
+        m_lineRenderer.transform.parent = transform;
     }
 
     void FixedUpdate()
@@ -23,6 +29,38 @@ public class BezierCurve : MonoBehaviour
 
             m_runners[i].DistanceTraveled += Time.deltaTime * _curveSpeed;
             m_runners[i].transform.position = GetPosiion(m_runners[i].DistanceTraveled);
+        }
+    }
+
+    public void UpdateLineRenderer()
+    {
+        if (_points.Length < 2)
+        {
+            return;
+        }
+
+        m_lineRenderer.SetVertexCount(_points.Length * 100);
+        m_lineRenderer.SetWidth(_lineWidth, _lineWidth);
+        m_lineRenderer.SetColors(_color, _color);
+
+        Vector3 currentPos = Vector3.zero;
+
+        for (int i = 0; i < _points.Length; i++)
+        {            
+            for (int j = 0; j < 101; j++)
+            {
+
+                if (i < _points.Length - 1)
+                {
+                    currentPos = Bezier.CubicBezier(_points[i].transform.position, _points[i]._frontBracket.position, _points[i + 1]._backBracket.position, _points[i + 1].transform.position, (j / 100f));
+                }
+                else if (_loop)
+                {
+                    currentPos = Bezier.CubicBezier(_points[i].transform.position, _points[i]._frontBracket.position, _points[0]._backBracket.position, _points[0].transform.position, (j / 100f));
+                }
+
+                m_lineRenderer.SetPosition((i * 100) + (j - 1), currentPos);
+            }
         }
     }
 
@@ -45,28 +83,6 @@ public class BezierCurve : MonoBehaviour
         else
         {
             return Bezier.CubicBezier(_points[currP].transform.position, _points[currP]._frontBracket.position, _points[0]._backBracket.position, _points[0].transform.position, realPercent);
-        }
-    }
-
-    public Vector3 GetConstantSpeed(float percent)
-    {
-        float realPercent = percent - (int)percent;
-        float step = _loop ? (float)(1f / _points.Length) : (float)(1f / (_points.Length - 1));
-
-        if (realPercent < 0) { realPercent = 1 - Mathf.Abs(realPercent); }
-
-        int currP = (int)(realPercent / step);
-        currP -= _loop ? _points.Length * (int)(currP / _points.Length) : (_points.Length - 1) * (int)(currP / (_points.Length - 1));
-
-        realPercent = (realPercent - (step * currP)) / step;
-
-        if (currP < _points.Length - 1)
-        {
-            return Bezier.CubicBezierSpeed(_points[currP].transform.position, _points[currP]._frontBracket.position, _points[currP + 1]._backBracket.position, _points[currP + 1].transform.position, realPercent);
-        }
-        else
-        {
-            return Bezier.CubicBezierSpeed(_points[currP].transform.position, _points[currP]._frontBracket.position, _points[0]._backBracket.position, _points[0].transform.position, realPercent);
         }
     }
 
